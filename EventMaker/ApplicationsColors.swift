6cc5ab1ca421e82f92e1b99012becfcc0b8ef8fc
Luiz Guerra <luizpedrofg@gmail.com>
2019-09-03 17:50:03 -0300

    Color  class handler

---
+++ EventMaker/ApplicationsColors.swift
@@ -0,0 +1,22 @@
+//
+//  ApplicationsColors.swift
+//  EventMaker
+//
+//  Created by Luiz Pedro Franciscatto Guerra on 03/09/19.
+//  Copyright © 2019 Raul Rodrigues. All rights reserved.
+//
+
+import UIKit
+
+class ApplicationsColors {
+    
+    static let DarkGray01 = UIColor(named: "Dark Gray")
+    static let DarkGray02 = UIColor(named: "Dark Gray 2")
+    static let LightGray01 = UIColor(named: "Light Gray")
+    static let LightGray02 = UIColor(named: "Light Gray 2")
+    static let Gray01 = UIColor(named: "Gray")
+    static let Gray02 = UIColor(named: "Gray 2")
+    static let VividBlueDisabled = UIColor(named: "Vivid Blue Disabled")
+    static let VividBlue = UIColor(named: "Vivid Blue")
+    
+}
