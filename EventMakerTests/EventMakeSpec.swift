e7d68e3605f808157998a908293b7d7019b343f2
Raul <raulferrodrigues@gmail.com>
2019-09-05 14:59:55 -0300

    gitflow-feature-stash: moreTests

--- EventMakerTests/EventMakeSpec.swift
+++ EventMakerTests/EventMakeSpec.swift
@@ -20,14 +20,14 @@ class EventMakeSpec: QuickSpec {
             events = EventDatabase(database: EventServiceTest.shared)
         }
         
-        describe("Tendo que eu ainda não o evento") {
-            context("quando eu criar o evento") {
-                it("entao o evento deve estar do database.") {
+        describe("Tendo que eu ainda não tenha criado o evento do churrasco,") {
+            context("quando eu criar o evento do churrasco") {
+                it("entao o evento deve estar na base de dados.") {
                     let event = Event(address: "",
                                       creator: "",
                                       date: "",
                                       hour: "",
-                                      description: "",
+                                      description: "Churrasco",
                                       isSharedPrice: false,
                                       name: "",
                                       participants: [],
@@ -41,11 +41,17 @@ class EventMakeSpec: QuickSpec {
             }
         }
         
-        describe("Tendo que eu já tenha criado o evento X") {
-            context("quando eu requisitar o evento X do servidor") {
+        describe("Tendo que eu já tenha criado o evento da canção") {
+            context("quando eu requisitar o evento da canção do servidor") {
                 it("entao o evento deve ser retornado na requisição.") {
                     events.database.getEvent(id: "wePMWXk8drRVDjxaVUmk") { (event) in
                         expect(event).toNot(beNil())
+                    }
+                }
+                
+                it("entao o nome do evento deve ser \"Ir para A Casa\"") {
+                    events.database.getEvent(id: "BCQeBZE9e3Im5PUPM6va") { (event) in
+                        expect(event?.name).to(equal("Ir para A Casa"))
                     }
                 }
             }
